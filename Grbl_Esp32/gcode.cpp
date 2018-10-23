79894a9379731645cca6a7899ff77acd8e4f7f7f
gflaser-au <43978849+gflaser-au@users.noreply.github.com>
2018-10-24 08:52:56 +1000

    Update gcode.cpp

--- Grbl_Esp32/gcode.cpp
+++ Grbl_Esp32/gcode.cpp
@@ -163,7 +163,10 @@ uint8_t gc_execute_line(char *line, uint8_t client)
               mantissa = 0; // Set to zero to indicate valid non-integer G command.
             }                
             break;
-          case 0: case 1: case 2: case 3: case 38:
+          case 0: case 1: case 2: case 3:
+#ifdef PROBE_PIN //only allow G38 "Probe" commands if a probe pin is defined.
+	  case 38: 
+#endif			
             // Check for G0/1/2/3/38 being called with G10/28/30/92 on same block.
             // * G43.1 is also an axis command but is not explicitly defined this way.
             if (axis_command) { FAIL(STATUS_GCODE_AXIS_COMMAND_CONFLICT); } // [Axis word/command conflict]
