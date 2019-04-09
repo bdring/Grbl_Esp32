d284707ed5522c0764ddbbb8ebf6580bd198bee3
bdring <barton.dring@gmail.com>
2019-04-09 10:18:50 -0500

    update MPCNC machine information
    Updated cpu_map for MPCNC machine
    Added default settings for MPCNC machine

--- Grbl_Esp32/defaults.h
+++ Grbl_Esp32/defaults.h
@@ -82,6 +82,58 @@
 
 #endif
 
+#ifdef DEFAULTS_MPCNC
+  // Grbl generic default settings. Should work across different machines.
+  #define DEFAULT_STEP_PULSE_MICROSECONDS 3 
+  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 255 //  255 = Keep steppers on 
+  
+  #define DEFAULT_STEPPING_INVERT_MASK 0 // uint8_t
+  #define DEFAULT_DIRECTION_INVERT_MASK 0 // uint8_t
+   #define DEFAULT_INVERT_ST_ENABLE 0 // boolean
+  #define DEFAULT_INVERT_LIMIT_PINS 1 // boolean
+  #define DEFAULT_INVERT_PROBE_PIN 0 // boolean 
+  
+  #define DEFAULT_STATUS_REPORT_MASK 2 // MPos enabled
+  
+  #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
+  #define DEFAULT_ARC_TOLERANCE 0.002 // mm
+  #define DEFAULT_REPORT_INCHES 0 // false
+  
+  #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
+  #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
+  
+  #define DEFAULT_HOMING_ENABLE 1  // false
+  #define DEFAULT_HOMING_DIR_MASK 3 // move positive dir Z,negative X,Y
+  #define DEFAULT_HOMING_FEED_RATE 600.0 // mm/min
+  #define DEFAULT_HOMING_SEEK_RATE 2000.0 // mm/min
+  #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
+  #define DEFAULT_HOMING_PULLOFF 1.5 // mm
+
+  #define DEFAULT_SPINDLE_RPM_MAX 1.0 // rpm used for spindle relay
+  #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
+  
+  #define DEFAULT_LASER_MODE 0 // false
+  
+  #define DEFAULT_X_STEPS_PER_MM 200.0
+  #define DEFAULT_Y_STEPS_PER_MM 200.0
+  #define DEFAULT_Z_STEPS_PER_MM 800.0
+  
+  #define DEFAULT_X_MAX_RATE 8000.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 8000.0 // mm/min
+  #define DEFAULT_Z_MAX_RATE 3000.0 // mm/min
+  
+  #define DEFAULT_X_ACCELERATION (200.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Y_ACCELERATION (200.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Z_ACCELERATION (100.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  
+  #define DEFAULT_X_MAX_TRAVEL 500.0 // mm NOTE: Must be a positive value.
+  #define DEFAULT_Y_MAX_TRAVEL 500.0 // mm NOTE: Must be a positive value.
+  #define DEFAULT_Z_MAX_TRAVEL 80.0 // mm NOTE: Must be a positive value.
+  
+
+#endif
+
+
 
 
 #endif
