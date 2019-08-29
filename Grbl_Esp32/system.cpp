24fc544b319ec3dc3d5a0d12c252e7bbb1ee228e
Luc <luc.lebosse@tech-hunters.com>
2019-08-29 18:23:53 +0200

    fix typo

--- Grbl_Esp32/system.cpp
+++ Grbl_Esp32/system.cpp
@@ -45,8 +45,8 @@ void system_ini() // Renamed from system_init() due to conflict with esp32 files
 
 #endif
    //customize pin definition if needed
-#if (GRBL_SPI_SS != -1) || (GRBL_SPI_MIS0 != -1) || (GRBL_SPI_MOSI != -1) || (GRBL_SPI_SCK != -1)
-    SPI.begin(GRBL_SPI_SCK, GRBL_SPI_MIS0, GRBL_SPI_MOSI, GRBL_SPI_SS);
+#if (GRBL_SPI_SS != -1) || (GRBL_SPI_MISO != -1) || (GRBL_SPI_MOSI != -1) || (GRBL_SPI_SCK != -1)
+    SPI.begin(GRBL_SPI_SCK, GRBL_SPI_MISO, GRBL_SPI_MOSI, GRBL_SPI_SS);
 #endif 
 }
 
