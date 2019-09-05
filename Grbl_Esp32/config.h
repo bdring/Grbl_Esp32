01f29b88cf3b7bba61f59749016293617fbf4cd6
Bart Dring <bdring@buildlog.net>
2019-09-05 12:03:26 -0500

    Returned to default cpu_map

--- Grbl_Esp32/config.h
+++ Grbl_Esp32/config.h
@@ -47,7 +47,7 @@ Some features should not be changed. See notes below.
 // one configuration file by placing their specific defaults and pin map at the bottom of this file.
 // If doing so, simply comment out these two defines and see instructions below.
 #define DEFAULTS_GENERIC
-#define CPU_MAP_POLAR_COASTER // these are defined in cpu_map.h
+#define CPU_MAP_ESP32 // these are defined in cpu_map.h
 #define VERBOSE_HELP // adds addition help info, but could confuse some senders
 
 
