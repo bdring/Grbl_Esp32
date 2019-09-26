86ee105be56357595b5b50515f29f2f5948b84bf
cprezzi <cprezzi@comsulting.ch>
2019-09-26 19:39:46 +0200

    Added #define HOMING_FORCE_POSITIVE_SPACE to force Grbl to always set the machine origin at bottom left.

--- Grbl_Esp32/config.h
+++ Grbl_Esp32/config.h
@@ -193,6 +193,9 @@ Some features should not be changed. See notes below.
 // define to force Grbl to always set the machine origin at the homed location despite switch orientation.
 // #define HOMING_FORCE_SET_ORIGIN // Uncomment to enable.
 
+// Uncomment this define to force Grbl to always set the machine origin at bottom left.
+#define HOMING_FORCE_POSITIVE_SPACE // Uncomment to enable.
+
 // Number of blocks Grbl executes upon startup. These blocks are stored in EEPROM, where the size
 // and addresses are defined in settings.h. With the current settings, up to 2 startup blocks may
 // be stored and executed in order. These startup blocks would typically be used to set the g-code
