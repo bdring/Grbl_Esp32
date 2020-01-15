cc78ba3afa65deaa809b5f6f59f85237cd10d13b
bdring <barton.dring@gmail.com>
2020-01-15 08:02:03 -0600

    Added $+ command to show extended $$ setting
    If you don't have SHOW_EXTENDED_SETTING defined, you can see those settings by sending $+

--- Grbl_Esp32/settings.h
+++ Grbl_Esp32/settings.h
@@ -75,11 +75,8 @@
 // #define SETTING_INDEX_G92    N_COORDINATE_SYSTEM+2  // Coordinate offset (G92.2,G92.3 not supported)
 
 // Define Grbl axis settings numbering scheme. Starts at START_VAL, every INCREMENT, over N_SETTINGS.
-#ifndef SHOW_EXTENDED_SETTINGS
-	#define AXIS_N_SETTINGS          4
-#else
-	#define AXIS_N_SETTINGS          8
-#endif
+#define AXIS_N_SETTINGS          8 // includes extended settings
+
 #define AXIS_SETTINGS_START_VAL  100 // NOTE: Reserving settings values >= 100 for axis settings. Up to 255.
 #define AXIS_SETTINGS_INCREMENT  10  // Must be greater than the number of axis settings
 
