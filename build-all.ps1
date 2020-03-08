71b2a58b9aa235234c3929a72bbc3291ff8b1f85
Mitch Bradley <wmb@firmworks.com>
2020-03-08 12:56:49 -1000

    Tweaks to build-all scripts

--- build-all.ps1
+++ build-all.ps1
@@ -4,31 +4,29 @@
 
 # Setting PYTHONIOENCODING avoids an obscure crash related to code page mismatch
 $env:PYTHONIOENCODING="utf-8"
-$envsave = $env:PLATFORMIO_BUILD_FLAGS
 
 Function BuildMachine($names) {
-     $basename = $names[0]
-     $addname = $names[1]
-     $env:PLATFORMIO_BUILD_FLAGS = "-DMACHINE_FILENAME=$basename"
-     $displayname = $basename
-     if ($addname -ne "") {
-          $env:PLATFORMIO_BUILD_FLAGS += " -DMACHINE_FILENAME2=$addname"
-          $displayname += " + $addname"
-      }
-     Write-Output "Building machine $displayname"
-     platformio run 2>&1 | Select-String error,Took
-     Write-Output " "
+    $basename = $names[0]
+    $addname = $names[1]
+    $env:PLATFORMIO_BUILD_FLAGS = "-DMACHINE_FILENAME=$basename"
+    $displayname = $basename
+    if ($addname -ne "") {
+        $env:PLATFORMIO_BUILD_FLAGS += " -DMACHINE_FILENAME2=$addname"
+        $displayname += " + $addname"
+    }
+    Write-Output "Building machine $displayname"
+    platformio run 2>&1 | Select-String error,Took
+    Write-Output " "
 }
 
 # First build all the base configurations with names that do not start with add_
 foreach ($filepath in Get-ChildItem -file .\Grbl_Esp32\Machines\* -Exclude add_*) {
-     BuildMachine($filepath.name, "")
+    BuildMachine($filepath.name, "")
 }
 
 # Then build all of the add-ons on top of a single base
 $base="3axis_v4.h"
 foreach ($filepath in Get-ChildItem -file .\Grbl_Esp32\Machines\add_*) {
-     BuildMachine($base, $filepath.name)
+    BuildMachine($base, $filepath.name)
 }
-
-$env:PLATFORMIO_BUILD_FLAGS=$envsave
+Remove-Item env:PLATFORMIO_BUILD_FLAGS
