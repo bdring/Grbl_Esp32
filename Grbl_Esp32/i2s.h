15c30f8eb0434cd1fe2e4db9cee45e9469b0ac2f
odaki <odaki@mars.dti.ne.jp>
2020-03-28 12:04:38 +0900

    Just copied from Marlin (first commit)

---
+++ Grbl_Esp32/i2s.h
@@ -0,0 +1,56 @@
+/*
+    i2s.h
+    Part of Grbl_ESP32
+
+    Header for basic GPIO expander using the ESP32 I2S peripheral
+
+    2020    - Michiyasu Odaki
+
+    Grbl_ESP32 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Grbl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include <stdint.h>
+
+// current value of the outputs provided over i2s
+extern volatile uint32_t i2s_port_data;
+
+int i2s_init();
+
+uint8_t i2s_state(uint8_t pin);
+
+void i2s_write(uint8_t pin, uint8_t val);
+
+void i2s_push_sample();
