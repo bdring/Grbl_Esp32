32e5429c458a98c1d8608396a5d2068b13be00b8
bdring <barton.dring@gmail.com>
2020-04-03 16:26:45 -0500

    Basic functionalty for testing.

--- Grbl_Esp32/grbl.h
+++ Grbl_Esp32/grbl.h
@@ -19,8 +19,8 @@
 */
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1f"
-#define GRBL_VERSION_BUILD "20200319"
+#define GRBL_VERSION "1.2a"
+#define GRBL_VERSION_BUILD "20200403"
 
 //#include <sdkconfig.h>
 #include <Arduino.h>
