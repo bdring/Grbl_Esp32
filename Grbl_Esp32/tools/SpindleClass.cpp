f9593975ce249dc89093375b5b5e3ff1ded181a0
bdring <barton.dring@gmail.com>
2020-04-03 16:26:45 -0500

    Basic functionalty for testing.

--- Grbl_Esp32/tools/SpindleClass.cpp
+++ Grbl_Esp32/tools/SpindleClass.cpp
@@ -1,8 +1,36 @@
 /*
+    SpindleClass.cpp
+
+    A Spindle Class
+    
+    Part of Grbl_ESP32
+    Copyright (c) 2014-2016 Sungeun K. Jeon for Gnea Research LLC
+
+    2020 -	Bart Dring This file was modified for use on the ESP32
+                    CPU. Do not use this with Grbl for atMega328P
+
+    Grbl is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Grbl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
+
+TODO
 
 
 Testing
-    Should $G show actual speed 
+    Should $G show actual speed (Regular Grbl does not)
+    Speed overrides only take place during motion
+
 */
 #include "grbl.h"
 #include "SpindleClass.h"
