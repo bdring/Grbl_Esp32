2af0a0734781adda88e3afc8927e03887258e0f2
bdring <barton.dring@gmail.com>
2020-04-08 11:23:26 -0500

    Got rid of spindle_control.cpp and spindle_control.h

--- Grbl_Esp32/grbl.h
+++ Grbl_Esp32/grbl.h
@@ -51,7 +51,7 @@
 #include "protocol.h"
 #include "report.h"
 #include "serial.h"
-#include "spindle_control.h"
+#include "tools/SpindleClass.h"
 #include "stepper.h"
 #include "jog.h"
 #include "inputbuffer.h"
