ddded794cba9a8c328196e5b675c7bd5a1e350be
bdring <barton.dring@gmail.com>
2020-04-23 17:19:17 -0500

    Return to default machine and
    Was setup for test machine and fast compile

--- Grbl_Esp32/machine.h
+++ Grbl_Esp32/machine.h
@@ -8,7 +8,7 @@
 
 // !!! For initial testing, start with test_drive.h which disables
 // all I/O pins
-#include "Machines/3axis_v4.h"
+#include "Machines/test_drive.h"
 
 // !!! For actual use, change the line above to select a board
 // from Machines/, for example:
