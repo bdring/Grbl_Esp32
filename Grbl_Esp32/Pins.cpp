89f00d126785711d277d8849169a505923d85847
odaki <odaki@mars.dti.ne.jp>
2020-06-06 12:42:18 +0900

    Add IRAM_ATTR attribute to new Arduino-compatible functions.

--- Grbl_Esp32/Pins.cpp
+++ Grbl_Esp32/Pins.cpp
@@ -20,13 +20,13 @@ const char* pinName(uint8_t pin) {
 // If we are using I2S, overload these standard Arduino
 // functions with versions that support I2S pins.
 // Otherwise the weak aliases that already exist will apply.
-void pinMode(uint8_t pin, uint8_t mode) {
+void IRAM_ATTR pinMode(uint8_t pin, uint8_t mode) {
     if (pin < I2S_OUT_PIN_BASE) {
         __pinMode(pin, mode);
     }
 }
 
-void digitalWrite(uint8_t pin, uint8_t val) {
+void IRAM_ATTR digitalWrite(uint8_t pin, uint8_t val) {
     if (pin < I2S_OUT_PIN_BASE) {
         __digitalWrite(pin, val);
         return;
@@ -34,7 +34,7 @@ void digitalWrite(uint8_t pin, uint8_t val) {
     i2s_out_write(pin - I2S_OUT_PIN_BASE, val);
 }
 
-int digitalRead(uint8_t pin) {
+int IRAM_ATTR digitalRead(uint8_t pin) {
     if (pin < I2S_OUT_PIN_BASE) {
         return __digitalRead(pin);
     }
