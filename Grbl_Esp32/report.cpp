fa67de91849a18d12b7be34c5d03dffa673dbdd7
bdring <barton.dring@gmail.com>
2020-06-30 21:26:55 -0500

    Fix crash due to large axis values
    Buffer for axis value was failing on "-10000.111" sized values.

--- Grbl_Esp32/report.cpp
+++ Grbl_Esp32/report.cpp
@@ -150,7 +150,7 @@ void grbl_notifyf(const char* title, const char* format, ...) {
 // formats axis values into a string and returns that string in rpt
 static void report_util_axis_values(float* axis_value, char* rpt) {
     uint8_t idx;
-    char axisVal[10];
+    char axisVal[20];
     float unit_conv = 1.0; // unit conversion multiplier..default is mm
     rpt[0] = '\0';
     if (report_inches->get())
