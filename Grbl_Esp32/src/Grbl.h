cb62f4b404f214e5533fe6dedf168ba7e665c8dd
bdring <barton.dring@gmail.com>
2020-08-23 07:07:34 -0500

    MPCNC Laser Module fix
    Some laser modules can fire with no signal. The level shift was enabled by spindle enable. This forces it on all the time so it does not float.

--- Grbl_Esp32/src/Grbl.h
+++ Grbl_Esp32/src/Grbl.h
@@ -23,8 +23,7 @@
 // Grbl versioning system
 
 #define GRBL_VERSION "1.3a"
-#define GRBL_VERSION_BUILD "20200819"
-
+#define GRBL_VERSION_BUILD "20200823"
 
 //#include <sdkconfig.h>
 #include <Arduino.h>
