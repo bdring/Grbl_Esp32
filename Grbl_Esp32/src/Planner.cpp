89fa85dac01cc3ba224d71d97e1e96770492cdab
Mitch Bradley <wmb@firmworks.com>
2020-09-05 08:28:59 -1000

    Tightened up sloppy returns (#577)
    * Tightened up sloppy returns
    
    No semantic change.
    
    * Added parentheses

--- Grbl_Esp32/src/Planner.cpp
+++ Grbl_Esp32/src/Planner.cpp
@@ -241,10 +241,7 @@ float plan_get_exec_block_exit_speed_sqr() {
 
 // Returns the availability status of the block ring buffer. True, if full.
 uint8_t plan_check_full_buffer() {
-    if (block_buffer_tail == next_buffer_head) {
-        return true;
-    }
-    return false;
+    return block_buffer_tail == next_buffer_head;
 }
 
 // Computes and returns block nominal speed based on running condition and override values.
