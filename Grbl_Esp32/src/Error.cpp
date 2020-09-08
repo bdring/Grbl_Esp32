58878f1584787b66c4a879174df3e17ef56b1c86
Mitch Bradley <wmb@firmworks.com>
2020-09-07 16:47:05 -1000

    Added license blocks to new files

--- Grbl_Esp32/src/Error.cpp
+++ Grbl_Esp32/src/Error.cpp
@@ -1,3 +1,24 @@
+/*
+  Error.cpp - Error names
+  Part of Grbl
+  Copyright (c) 2014-2016 Sungeun K. Jeon for Gnea Research LLC
+
+	2018 -	Bart Dring This file was modifed for use on the ESP32
+					CPU. Do not use this with Grbl for atMega328P
+        2020 - Mitch Bradley
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "Error.h"
 
 std::map<Error, const char*> ErrorCodes = {
--- Grbl_Esp32/src/Error.h
+++ Grbl_Esp32/src/Error.h
@@ -1,7 +1,29 @@
 #pragma once
+
+/*
+  Error.h - Error numbers
+  Part of Grbl
+  Copyright (c) 2014-2016 Sungeun K. Jeon for Gnea Research LLC
+
+	2018 -	Bart Dring This file was modifed for use on the ESP32
+					CPU. Do not use this with Grbl for atMega328P
+        2020 - Mitch Bradley
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <map>
 
-// Define Grbl error codes. Valid values (0-255)
+// Grbl error codes. Valid values (0-255)
 enum class Error : uint8_t {
     Ok                          = 0,
     ExpectedCommandLetter       = 1,
