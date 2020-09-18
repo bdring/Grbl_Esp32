515bccf52e5645726895338ad471a1cc4e8098ea
bdring <barton.dring@gmail.com>
2020-09-17 21:20:04 -0500

    Cleanup machine defs (#602)

--- Grbl_Esp32/src/Machines/6_pack_stepstick_v1.h
+++ Grbl_Esp32/src/Machines/6_pack_stepstick_v1.h
@@ -24,14 +24,7 @@
 */
 #define MACHINE_NAME            "6 Pack Controller V1 (StepStick)"
 
-#ifdef N_AXIS
-        #undef N_AXIS
-#endif
 #define N_AXIS 6
-
-#ifdef ENABLE_SD_CARD
-    #undef ENABLE_SD_CARD
-#endif
 
 // === Special Features
 
@@ -56,26 +49,32 @@
 
 #define STEPPER_RESET           GPIO_NUM_19
 
+// Motor Socket #1
 #define X_DISABLE_PIN           I2SO(0)
 #define X_DIRECTION_PIN         I2SO(1)
 #define X_STEP_PIN              I2SO(2)
 
+// Motor Socket #2
 #define Y_DIRECTION_PIN         I2SO(4)
 #define Y_STEP_PIN              I2SO(5)
 #define Y_DISABLE_PIN           I2SO(7)
 
+// Motor Socket #3
 #define Z_DISABLE_PIN           I2SO(8)
 #define Z_DIRECTION_PIN         I2SO(9)
 #define Z_STEP_PIN              I2SO(10)
 
+// Motor Socket #4
 #define A_DIRECTION_PIN         I2SO(12)
 #define A_STEP_PIN              I2SO(13)
 #define A_DISABLE_PIN           I2SO(15)
 
+// Motor Socket #5
 #define B_DISABLE_PIN           I2SO(16)
 #define B_DIRECTION_PIN         I2SO(17)
 #define B_STEP_PIN              I2SO(18)
 
+// Motor Socket #5
 #define C_DIRECTION_PIN         I2SO(20)
 #define C_STEP_PIN              I2SO(21)
 #define C_DISABLE_PIN           I2SO(23)
@@ -86,55 +85,49 @@
     The list of modules is here...
     https://github.com/bdring/6-Pack_CNC_Controller/wiki/CNC-I-O-Module-List
     Click on each module to get example for using the modules in the sockets
+
+
+Socket #1
+#1 GPIO_NUM_33 
+#2 GPIO_NUM_32
+#3 GPIO_NUM_35 (input only)
+#4 GPIO_NUM_34 (input only)
+
+Socket #2
+#1 GPIO_NUM_2
+#2 GPIO_NUM_25
+#3 GPIO_NUM_39 (input only)
+#4 GPIO_NUM_36 (input only)
+
+Socket #3
+#1 GPIO_NUM_26
+#2 GPIO_NUM_4
+#3 GPIO_NUM_16
+#4 GPIO_NUM_27
+
+Socket #4
+#1 GPIO_NUM_14
+#2 GPIO_NUM_13
+#3 GPIO_NUM_15
+#4 GPIO_NUM_12
+
+Socket #5
+#1 I2SO(24)  (output only)
+#2 I2SO(25)  (output only)
+#3 I2SO26)  (output only)
+
 */
 
- 
-// Socket #1
-// #1 GPIO_NUM_33 
-// #2 GPIO_NUM_32
-// #3 GPIO_NUM_35 (input only)
-// #4 GPIO_NUM_34 (input only)
-
-
-// Socket #2
-// #1 GPIO_NUM_2
-// #2 GPIO_NUM_25
-// #3 GPIO_NUM_39 (input only)
-// #4 GPIO_NUM_36 (input only)
-
-// Socket #3
-// #1 GPIO_NUM_26
-// #2 GPIO_NUM_4
-// #3 GPIO_NUM_16
-// #4 GPIO_NUM_27
-
-
-// Socket #4
-// #1 GPIO_NUM_14
-// #2 GPIO_NUM_13
-// #3 GPIO_NUM_15
-// #4 GPIO_NUM_12
-
-
-// Socket #5
-// #1 GPIO_NUM_24  (output only)
-// #2 GPIO_NUM_25  (output only)
-// #3 GPIO_NUM_26  (output only)
-// #4 GPIO_NUM_27  (output only)
- 
+// 4x Input Module in Socket #1
+// https://github.com/bdring/6-Pack_CNC_Controller/wiki/4x-Switch-Input-module
 #define X_LIMIT_PIN             GPIO_NUM_33
 #define Y_LIMIT_PIN             GPIO_NUM_32
 #define Z_LIMIT_PIN             GPIO_NUM_35
-//#define A_LIMIT_PIN             GPIO_NUM_34
-//#define B_LIMIT_PIN             GPIO_NUM_39
-//#define C_LIMIT_PIN             GPIO_NUM_36
-
 #define PROBE_PIN               GPIO_NUM_34
 
 
-
-
 // 5V output CNC module in socket #3
+// https://github.com/bdring/6-Pack_CNC_Controller/wiki/4x-5V-Buffered-Output-Module
 #define SPINDLE_TYPE            SpindleType::PWM
 #define SPINDLE_OUTPUT_PIN      GPIO_NUM_26
 #define SPINDLE_ENABLE_PIN      GPIO_NUM_4
