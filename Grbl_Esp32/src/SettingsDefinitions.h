c656a62d41e4a573051f90dde2176cc86540c2fa
bdring <barton.dring@gmail.com>
2020-09-24 18:16:47 -0500

    Homing cycle settings (#613)
    * Initial Tests Complete
    
    * Update Grbl.h
    
    * Update variables
    
    Co-authored-by: Mitch Bradley <wmb@firmworks.com>

--- Grbl_Esp32/src/SettingsDefinitions.h
+++ Grbl_Esp32/src/SettingsDefinitions.h
@@ -23,6 +23,7 @@ extern AxisMaskSetting* step_invert_mask;
 extern AxisMaskSetting* dir_invert_mask;
 extern AxisMaskSetting* homing_dir_mask;
 extern AxisMaskSetting* homing_squared_axes;
+extern AxisMaskSetting* homing_cycle[MAX_N_AXIS];
 
 extern FlagSetting* step_enable_invert;
 extern FlagSetting* limit_invert;
