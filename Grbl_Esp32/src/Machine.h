21422a8c633677956e00452546481d85e73394f2
bdring <barton.dring@gmail.com>
2020-10-05 18:38:22 -0500

    Wip

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -8,7 +8,7 @@
 // !!! For initial testing, start with test_drive.h which disables
 // all I/O pins
 // #include "Machines/atari_1020.h"
-#    include "Machines/tapster_pro_6P_trinamic.h"
+#    include "Machines/tapster3.h"
 
 // !!! For actual use, change the line above to select a board
 // from Machines/, for example:
