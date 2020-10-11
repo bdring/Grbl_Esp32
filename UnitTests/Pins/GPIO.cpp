39ed7dfbc4f1a5a833b2c311a022d9dd58dcf8b9
Stefan de Bruijn <stefan@nubilosoft.com>
2020-10-11 17:12:48 +0200

    Fixed a bug in pin class. Added some unit tests for GPIO pins

--- UnitTests/Pins/GPIO.cpp
+++ UnitTests/Pins/GPIO.cpp
@@ -11,7 +11,7 @@ namespace Pins {
         return pin;
     }
 
-    TEST(Read, GPIO) {
+    TEST(ReadInputPin, GPIO) {
         auto pin = Init();
 
         pin.setAttr(Pin::Attr::Input);
@@ -22,11 +22,18 @@ namespace Pins {
         ASSERT_EQ(true, pin.read());
     }
 
+    TEST(ReadOutputPin, GPIO) {
+        auto pin = Init();
+
+        pin.setAttr(Pin::Attr::Output);
+        ASSERT_ANY_THROW(pin.read());
+    }
+
     TEST(WriteInputPin, GPIO) {
         auto pin = Init();
 
         pin.setAttr(Pin::Attr::Input);
-        EXPECT_ANY_THROW(pin.on());
+        ASSERT_ANY_THROW(pin.on());
     }
 
     TEST(WriteOutputPin, GPIO) {
@@ -42,4 +49,75 @@ namespace Pins {
         pin.off();
         ASSERT_EQ(false, SoftwareGPIO::instance().get(16));
     }
+
+    TEST(ReadIOPin, GPIO) {
+        auto pin = Init();
+
+        pin.setAttr(Pin::Attr::Output | Pin::Attr::Input);
+        ASSERT_EQ(false, pin.read());
+        ASSERT_EQ(false, SoftwareGPIO::instance().get(16));
+
+        pin.on();
+        ASSERT_EQ(true, pin.read());
+        ASSERT_EQ(true, SoftwareGPIO::instance().get(16));
+
+        pin.off();
+        ASSERT_EQ(false, pin.read());
+        ASSERT_EQ(false, SoftwareGPIO::instance().get(16));
+    }
+
+    void TestISR(int deltaRising, int deltaFalling, int mode) {
+        auto pin = Init();
+        pin.setAttr(Pin::Attr::Input | Pin::Attr::Output | Pin::Attr::ISR);
+
+        int hitCount = 0;
+        int expected = 0;
+        pin.attachInterrupt(
+            [](void* arg) {
+                int* hc = static_cast<int*>(arg);
+                ++(*hc);
+            },
+            mode,
+            &hitCount);
+
+        // Two ways to set I/O:
+        // 1. using on/off
+        // 2. external source (e.g. set softwareio pin value)
+        //
+        // We read as well, because that shouldn't modify the state.
+
+        pin.on();
+        expected += deltaRising;
+        ASSERT_EQ(hitCount, expected);
+        ASSERT_EQ(true, pin.read());
+
+        pin.off();
+        expected += deltaFalling;
+        ASSERT_EQ(hitCount, expected);
+        ASSERT_EQ(false, pin.read());
+
+        SoftwareGPIO::instance().set(16, true);
+        expected += deltaRising;
+        ASSERT_EQ(hitCount, expected);
+        ASSERT_EQ(true, pin.read());
+
+        SoftwareGPIO::instance().set(16, false);
+        expected += deltaFalling;
+        ASSERT_EQ(hitCount, expected);
+        ASSERT_EQ(false, pin.read());
+
+        // Detach interrupt. Regardless of what we do, it shouldn't change hitcount anymore.
+        pin.detachInterrupt();
+        pin.on();
+        pin.off();
+        SoftwareGPIO::instance().set(16, true);
+        SoftwareGPIO::instance().set(16, false);
+        ASSERT_EQ(hitCount, expected);
+    }
+
+    TEST(ISRRisingPin, GPIO) { TestISR(1, 0, RISING); }
+
+    TEST(ISRFallingPin, GPIO) { TestISR(0, 1, FALLING); }
+
+    TEST(ISRChangePin, GPIO) { TestISR(1, 1, CHANGE); }
 }
--- UnitTests/test.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
