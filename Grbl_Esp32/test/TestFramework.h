10822e34043dc9616c4526aa54de9f08e1ad512d
Stefan de Bruijn <stefan@nubilosoft.com>
2020-10-17 14:44:37 +0200

    Fixed names and hierarchy in Visual Studio test explorer.

--- Grbl_Esp32/test/TestFramework.h
+++ Grbl_Esp32/test/TestFramework.h
@@ -87,7 +87,32 @@ inline void PrintSerial(const char* format, ...) {
 #    undef ASSERT_NO_THROW
 #    undef ASSERT_ANY_THROW
 
-#    define Test(test_case_name, test_name) GTEST_TEST(test_case_name, test_name)
+#    define Test(test_case_name, test_name) Test2(test_case_name, test_name, ::testing::Test, ::testing::internal::GetTestTypeId())
+
+#    define Test2(test_case_name, test_name, parent_class, parent_id) \
+class GTEST_TEST_CLASS_NAME_(test_case_name, test_name) : public parent_class {\
+ public:\
+  GTEST_TEST_CLASS_NAME_(test_case_name, test_name)() {}\
+ private:\
+  virtual void TestBody();\
+  static ::testing::TestInfo* const test_info_ GTEST_ATTRIBUTE_UNUSED_;\
+  GTEST_DISALLOW_COPY_AND_ASSIGN_(\
+      GTEST_TEST_CLASS_NAME_(test_case_name, test_name));\
+};\
+\
+::testing::TestInfo* const GTEST_TEST_CLASS_NAME_(test_case_name, test_name)\
+  ::test_info_ =\
+    ::testing::internal::MakeAndRegisterTestInfo(\
+        #test_name "." #test_case_name, #test_name, NULL, NULL, \
+        ::testing::internal::CodeLocation(__FILE__, __LINE__), \
+        (parent_id), \
+        parent_class::SetUpTestCase, \
+        parent_class::TearDownTestCase, \
+        new ::testing::internal::TestFactoryImpl<\
+            GTEST_TEST_CLASS_NAME_(test_case_name, test_name)>);\
+void GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::TestBody()
+
+
 
 #    define NativeTest(testCase, testName) Test(testCase, testName)
 #    define PlatformTest(testCase, testName) TEST_INST_NAME(testCase, testName)
