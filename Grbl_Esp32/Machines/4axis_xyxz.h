fac83a8bf76055d3b8b28691bb90a8284b3265c9
Jens Hauser <44340656+JensHauser@users.noreply.github.com>
2020-12-22 00:26:18 +0100

    Tets Fräse 2.6 ok

--- Grbl_Esp32/Machines/4axis_xyxz.h
+++ Grbl_Esp32/Machines/4axis_xyxz.h
@@ -9,47 +9,51 @@
 #undef N_AXIS
 #endif
 
- // Moechte ich mit 3 oder 4 Achsen arbeiten?
- // Beides ist eingerichtet.
+/*
+ * Moechte ich mit 3 oder 4 Achsen arbeiten?
+ * Beides ist eingerichtet.
+ */
+// ######################################
 #define N_AXIS 3
+// #######################################
 
 //#define CUSTOM_CODE_FILENAME "Custom/4axis_xyxz.cpp"
 
 /*
- * Zuordnung Schrittmotoren
+ * Spindeltyp definieren, ich nutze ein Relais
  */
-#define X_STEP_PIN              GPIO_NUM_12
-#define X_DIRECTION_PIN         GPIO_NUM_14
-#define Y_STEP_PIN              GPIO_NUM_26
-#define Y_DIRECTION_PIN         GPIO_NUM_15
-#define Z_STEP_PIN              GPIO_NUM_27
-#define Z_DIRECTION_PIN         GPIO_NUM_33
-
-#define STEPPERS_DISABLE_PIN    GPIO_NUM_13 //ok
-
- // Falls die 4. Achse genutzt wird
-#if (N_AXIS == 4)
-#define A_STEP_PIN              GPIO_NUM_25
-#define A_DIRECTION_PIN         GPIO_NUM_22
-#endif
-
 #define SPINDLE_TYPE SpindleType::RELAY
-//#define SPINDLE_ENABLE_PIN      GPIO_NUM_32
-#define SPINDLE_OUTPUT_PIN GPIO_NUM_32
-
+#define SPINDLE_OUTPUT_PIN GPIO_NUM_3
 
 /*
- * Zuordnung Endschalter
- * The 1 bits in LIMIT_MASK set the axes that have limit switches
- * X, Y, Z, A do, the LIMIT_MASK value would be B1111
+ * Zuordnung Schrittmotoren
+ * Analog zu den Pins des Boards. Am Besten in KICAD ablesen.
+ * Hier wird Version "Fräse 2.6" genutzt
  */
-#define X_LIMIT_PIN             GPIO_NUM_17 //ok
-#define Y_LIMIT_PIN             GPIO_NUM_4  //ok
-#define Z_LIMIT_PIN             GPIO_NUM_16 //ok
+#define STEPPERS_DISABLE_PIN    GPIO_NUM_13 //ok
 
- // Falls die 4. Achse genutzt wird
+#define X_STEP_PIN              GPIO_NUM_12
+#define X_DIRECTION_PIN         GPIO_NUM_14
+#define X_LIMIT_PIN             GPIO_NUM_17
+
+#define Y_STEP_PIN              GPIO_NUM_26
+#define Y_DIRECTION_PIN         GPIO_NUM_15
+#define Y_LIMIT_PIN             GPIO_NUM_4
+
+#define Z_STEP_PIN              GPIO_NUM_27
+#define Z_DIRECTION_PIN         GPIO_NUM_33
+#define Z_LIMIT_PIN             GPIO_NUM_16
+
+// Falls die 4. Achse genutzt wird
 #if (N_AXIS == 4)
-    #define A_LIMIT_PIN         GPIO_NUM_21 //ok
+    #define A_STEP_PIN          GPIO_NUM_25
+    #define A_DIRECTION_PIN     GPIO_NUM_22
+    #define A_LIMIT_PIN         GPIO_NUM_21
+
+    /* Zuordnung Endschalter
+     * The 1 bits in LIMIT_MASK set the axes that have limit switches
+     * X, Y, Z, A do, the LIMIT_MASK value would be B1111
+     */
     #define LIMIT_MASK          B1111
 #else
     // bei 3 Achsen
@@ -64,6 +68,8 @@
 #define CONTROL_RESET_PIN       GPIO_NUM_34  // needs external pullup
 #define CONTROL_FEED_HOLD_PIN   GPIO_NUM_36  // needs external pullup
 #define CONTROL_CYCLE_START_PIN GPIO_NUM_39  // needs external pullup
+
+#define COOLANT_FLOOD_PIN       GPIO_NUM_2
 
 /* Normally Grbl_ESP32 ignores tool changes.
  * It just tracks the current tool number.
