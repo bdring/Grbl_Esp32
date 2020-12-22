ecd1b9953b9e69975af02e241ca0345b800e158d
Mitch Bradley <wmb@firmworks.com>
2020-12-22 07:37:01 -1000

    Eliminated . metacharacter

--- Grbl_Esp32/src/Regex.h
+++ Grbl_Esp32/src/Regex.h
@@ -1,11 +1,5 @@
-// Simple regular expression matcher from Rob Pike per
-// https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
-
-//    c    matches any literal character c
-//    .    matches any single character
-//    ^    matches the beginning of the input string
-//    $    matches the end of the input string
-//    *    matches zero or more occurrences of the previous character
+// Simple regular expression matcher.
+// See Regex.cpp for attribution, description and discussion
 
 // Returns true if text contains the regular expression regexp
 bool regexMatch(const char* regexp, const char* text);
