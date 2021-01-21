dd7777184290c1c564aac973bd4b22e51d5d71bd
bdring <barton.dring@gmail.com>
2021-01-21 16:12:22 -0600

    Update build date

--- Grbl_Esp32/src/Grbl.h
+++ Grbl_Esp32/src/Grbl.h
@@ -23,7 +23,7 @@
 // Grbl versioning system
 
 const char* const GRBL_VERSION       = "1.3a";
-const char* const GRBL_VERSION_BUILD = "20210101";
+const char* const GRBL_VERSION_BUILD = "20210121";
 
 //#include <sdkconfig.h>
 #include <Arduino.h>
