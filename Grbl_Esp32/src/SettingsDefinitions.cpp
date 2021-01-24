39dbc62a656270c69e45fa854c359aa683a8a38b
bdring <barton.dring@gmail.com>
2021-01-24 15:24:54 -0600

    Added message level setting

--- Grbl_Esp32/src/SettingsDefinitions.cpp
+++ Grbl_Esp32/src/SettingsDefinitions.cpp
@@ -57,6 +57,8 @@ IntSetting*   spindle_pwm_bit_precision;
 
 EnumSetting* spindle_type;
 
+EnumSetting* message_level;
+
 enum_opt_t spindleTypes = {
     // clang-format off
     { "NONE", int8_t(SpindleType::NONE) },
@@ -69,6 +71,17 @@ enum_opt_t spindleTypes = {
     { "10V", int8_t(SpindleType::_10V) },
     { "H2A", int8_t(SpindleType::H2A) },
     // clang-format on
+};
+
+enum_opt_t messageLevels = {
+    // clang-format off
+    { "None", int8_t(MsgLevel::None) },
+    { "Error", int8_t(MsgLevel::Error) },
+    { "Warning", int8_t(MsgLevel::Warning) },
+    { "Info", int8_t(MsgLevel::Info) },
+    { "Debug", int8_t(MsgLevel::Debug) },
+    { "Verbose", int8_t(MsgLevel::Verbose) },
+       // clang-format on
 };
 
 AxisSettings* x_axis_settings;
@@ -256,16 +269,16 @@ void make_settings() {
     b_axis_settings = axis_settings[B_AXIS];
     c_axis_settings = axis_settings[C_AXIS];
     for (axis = MAX_N_AXIS - 1; axis >= 0; axis--) {
-        def = &axis_defaults[axis];
-        auto setting =
-            new IntSetting(EXTENDED, WG, makeGrblName(axis, 170), makename(def->name, "StallGuard"), def->stallguard, -64, 255, postMotorSetting);
+        def          = &axis_defaults[axis];
+        auto setting = new IntSetting(
+            EXTENDED, WG, makeGrblName(axis, 170), makename(def->name, "StallGuard"), def->stallguard, -64, 255, postMotorSetting);
         setting->setAxis(axis);
         axis_settings[axis]->stallguard = setting;
     }
     for (axis = MAX_N_AXIS - 1; axis >= 0; axis--) {
-        def = &axis_defaults[axis];
-        auto setting =
-            new IntSetting(EXTENDED, WG, makeGrblName(axis, 160), makename(def->name, "Microsteps"), def->microsteps, 0, 256, postMotorSetting);
+        def          = &axis_defaults[axis];
+        auto setting = new IntSetting(
+            EXTENDED, WG, makeGrblName(axis, 160), makename(def->name, "Microsteps"), def->microsteps, 0, 256, postMotorSetting);
         setting->setAxis(axis);
         axis_settings[axis]->microsteps = setting;
     }
@@ -395,4 +408,6 @@ void make_settings() {
     user_macro2 = new StringSetting(EXTENDED, WG, NULL, "User/Macro2", DEFAULT_USER_MACRO2);
     user_macro1 = new StringSetting(EXTENDED, WG, NULL, "User/Macro1", DEFAULT_USER_MACRO1);
     user_macro0 = new StringSetting(EXTENDED, WG, NULL, "User/Macro0", DEFAULT_USER_MACRO0);
+
+    message_level = +new EnumSetting(NULL, EXTENDED, WG, NULL, "Message/Level", static_cast<int8_t>(MsgLevel::Info), &messageLevels, NULL);
 }
