5bc3cf819b27b892c46a1394ebbb1798b2feee8e
Florian Ragwitz <florian.ragwitz@gmail.com>
2021-02-19 09:12:21 -0800

    Configure motors after I/O pins (#742)
    So machine definitions can change the SPI pins before we talk to
    any Trinamic drivers.

--- Grbl_Esp32/src/Grbl.cpp
+++ Grbl_Esp32/src/Grbl.cpp
@@ -39,8 +39,8 @@ void grbl_init() {
 #endif
     settings_init();  // Load Grbl settings from non-volatile storage
     stepper_init();   // Configure stepper pins and interrupt timers
-    init_motors();
     system_ini();  // Configure pinout pins and pin-change interrupt (Renamed due to conflict with esp32 files)
+    init_motors();
     memset(sys_position, 0, sizeof(sys_position));  // Clear machine position.
     machine_init();                                 // weak definition in Grbl.cpp does nothing
     // Initialize system state.
