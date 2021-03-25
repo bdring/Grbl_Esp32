e8cf0bbfccb944d1cda8a8da3111e0d77f2993f1
marcosprojects <marco1601@web.de>
2021-03-25 20:53:09 +0100

    Yalang YL620 VFD (#838)
    * New SpindleType YL620
    
    Files for new SpindleType Yalang 620. So far the contents are a duplicate of H2ASpindle.h and H2ASpindle.cpp
    
    * Added register documentation and implemented read and write data packets
    
    * Some fixes, mostly regarding RX packet length

--- Grbl_Esp32/src/Spindles/Spindle.cpp
+++ Grbl_Esp32/src/Spindles/Spindle.cpp
@@ -38,6 +38,7 @@
 #include "H2ASpindle.h"
 #include "BESCSpindle.h"
 #include "10vSpindle.h"
+#include "YL620Spindle.h"
 
 namespace Spindles {
     // An instance of each type of spindle is created here.
@@ -51,6 +52,7 @@ namespace Spindles {
     H2A      h2a;
     BESC     besc;
     _10v     _10v;
+    YL620   yl620;
 
     void Spindle::select() {
         switch (static_cast<SpindleType>(spindle_type->get())) {
@@ -77,6 +79,9 @@ namespace Spindles {
                 break;
             case SpindleType::H2A:
                 spindle = &h2a;
+                break;
+            case SpindleType::YL620:
+                spindle = &yl620;
                 break;
             case SpindleType::NONE:
             default:
