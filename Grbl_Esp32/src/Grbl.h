e1ffdead437f93059fe3c71ed54515ba441980c6
Stefan de Bruijn <stefan@nubilosoft.com>
2021-03-26 20:12:10 +0100

    Added a bit of version handling.

--- Grbl_Esp32/src/Grbl.h
+++ Grbl_Esp32/src/Grbl.h
@@ -22,8 +22,8 @@
 
 // Grbl versioning system
 
-const char* const GRBL_VERSION       = "1.3a";
-const char* const GRBL_VERSION_BUILD = "20201101";
+const char* const GRBL_VERSION       = "2.1a";
+const char* const GRBL_VERSION_BUILD = "20210326";
 
 //#include <sdkconfig.h>
 #include <Arduino.h>
