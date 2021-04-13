bb3ce85fc47189792238ecc47c455a8962b36b0d
Mitch Bradley <wmb@firmworks.com>
2021-04-13 11:18:22 -1000

    REVERT_TO_ARDUINO_SERIAL off by default

--- Grbl_Esp32/src/Serial.cpp
+++ Grbl_Esp32/src/Serial.cpp
@@ -57,7 +57,11 @@
 
 #include "Grbl.h"
 
-#define REVERT_TO_SERIAL
+// Define this to use the Arduino serial (UART) driver instead
+// of the one in Uart.cpp, which uses the ESP-IDF UART driver.
+// This is for regression testing, and can be removed after
+// testing is complete.
+// #define REVERT_TO_ARDUINO_SERIAL
 
 portMUX_TYPE myMutex = portMUX_INITIALIZER_UNLOCKED;
 
@@ -67,7 +71,7 @@ WebUI::InputBuffer client_buffer[CLIENT_COUNT];  // create a buffer for each cli
 
 // Returns the number of bytes available in a client buffer.
 uint8_t client_get_rx_buffer_available(uint8_t client) {
-#ifdef REVERT_TO_SERIAL
+#ifdef REVERT_TO_ARDUINO_SERIAL
     return 128 - Serial.available();
 #else
     return 128 - Uart0.available();
@@ -98,7 +102,7 @@ void client_init() {
     xTaskCreatePinnedToCore(heapCheckTask, "heapTask", 2000, NULL, 1, NULL, 1);
 #endif
 
-#ifdef REVERT_TO_SERIAL
+#ifdef REVERT_TO_ARDUINO_SERIAL
     Serial.begin(BAUD_RATE, SERIAL_8N1, 3, 1, false);
     client_reset_read_buffer(CLIENT_ALL);
     Serial.write("\r\n");  // create some white space after ESP32 boot info
@@ -126,7 +130,7 @@ void client_init() {
 
 static uint8_t getClientChar(uint8_t* data) {
     int res;
-#ifdef REVERT_TO_SERIAL
+#ifdef REVERT_TO_ARDUINO_SERIAL
     if (client_buffer[CLIENT_SERIAL].availableforwrite() && (res = Serial.read()) != -1) {
 #else
     if (client_buffer[CLIENT_SERIAL].availableforwrite() && (res = Uart0.read()) != -1) {
@@ -362,7 +366,7 @@ void client_write(uint8_t client, const char* text) {
     }
 #endif
     if (client == CLIENT_SERIAL || client == CLIENT_ALL) {
-#ifdef REVERT_TO_SERIAL
+#ifdef REVERT_TO_ARDUINO_SERIAL
         Serial.write(text);
 #else
         Uart0.write(text);
