00a718c11a4f451140f684bd21fbcc98964a4811
Stefan de Bruijn <stefan@nubilosoft.com>
2021-06-01 20:19:07 +0200

    Implemented JSON generator for WebUI [untested]

---
+++ Grbl_Esp32/src/Configuration/JsonGenerator.h
@@ -0,0 +1,60 @@
+/*
+    Part of Grbl_ESP32
+    2021 -  Stefan de Bruijn
+
+    Grbl_ESP32 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Grbl_ESP32 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <vector>
+
+#include "../Pin.h"
+#include "../StringRange.h"
+#include "../StringStream.h"
+#include "HandlerBase.h"
+
+#include "../WebUI/JSONEncoder.h"
+
+namespace Configuration {
+    class Configurable;
+
+    class JsonGenerator : public HandlerBase {
+        JsonGenerator(const JsonGenerator&) = delete;
+        JsonGenerator& operator=(const JsonGenerator&) = delete;
+
+        char  _currentPath[256];  // 256 = max size of configuration string.
+        char* _paths[16];         // 16 = max depth. Points to the _end_ of a string in currentPath
+        int   _depth;             // current depth. Used along with _paths
+        WebUI::JSONencoder& _encoder;
+
+        void enter(const char* name);
+        void add(Configuration::Configurable* configurable);
+        void leave();
+
+    protected:
+        void        handleDetail(const char* name, Configurable* value) override;
+        bool        matchesUninitialized(const char* name) override { return false; }
+        HandlerType handlerType() override { return HandlerType::Generator; }
+
+    public:
+        JsonGenerator(WebUI::JSONencoder& encoder);
+
+        void handle(const char* name, int& value, int32_t minValue, int32_t maxValue) override;
+        void handle(const char* name, double& value, double minValue, double maxValue) override;
+        void handle(const char* name, StringRange& value, int minLength, int maxLength) override;
+        void handle(const char* name, Pin& value) override;
+        void handle(const char* name, int& value, EnumItem* e) override;
+    };
+}
