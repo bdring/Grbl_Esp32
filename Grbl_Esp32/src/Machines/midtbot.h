722f8c382e60e59039474c372cdb4fa35f7bac7c
odaki <odaki@mars.dti.ne.jp>
2021-06-06 18:48:30 +0900

    Prevent unintended PenDown
    Since midTbot's Z axis is servo, I consider Z axis homing unnecessary.

--- Grbl_Esp32/src/Machines/midtbot.h
+++ Grbl_Esp32/src/Machines/midtbot.h
@@ -49,9 +49,8 @@
 #define SPINDLE_TYPE SpindleType::NONE
 
 // defaults
-#define DEFAULT_HOMING_CYCLE_0      bit(Z_AXIS)
-#define DEFAULT_HOMING_CYCLE_1      bit(Y_AXIS)
-#define DEFAULT_HOMING_CYCLE_2      bit(X_AXIS)
+#define DEFAULT_HOMING_CYCLE_0      bit(Y_AXIS)
+#define DEFAULT_HOMING_CYCLE_1      bit(X_AXIS)
 #define DEFAULT_HOMING_DIR_MASK     (bit(X_AXIS) | bit (Z_AXIS)) // these home negative
 
 #define DEFAULT_STEP_PULSE_MICROSECONDS 3
