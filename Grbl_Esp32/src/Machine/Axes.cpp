bdring/Grbl_Esp32

commit 1e7b468fb9d4a207d2dc36734f975e345ca4efc5

Axes skipped the validate phase

Grbl_Esp32/src/Machine/Axes.cpp
@@ -208,7 +208,8 @@ namespace Machine {
 
             if (handler.handlerType() == Configuration::HandlerType::Runtime || handler.handlerType() == Configuration::HandlerType::Parser ||
                 handler.handlerType() == Configuration::HandlerType::AfterParse ||
-                handler.handlerType() == Configuration::HandlerType::Generator) {
+                handler.handlerType() == Configuration::HandlerType::AfterParse ||
+                handler.handlerType() == Configuration::HandlerType::Validator) {
                 handler.section(tmp, _axis[a]);
             }
         }

commit 75b2255dce803ec12fe34129862df2d9001c2740

Fixed $cd for axes
A typo in 1e7b468fb broke it

Grbl_Esp32/src/Machine/Axes.cpp
@@ -209,6 +209,7 @@ namespace Machine {
             if (handler.handlerType() == Configuration::HandlerType::Runtime || handler.handlerType() == Configuration::HandlerType::Parser ||
                 handler.handlerType() == Configuration::HandlerType::AfterParse ||
                 handler.handlerType() == Configuration::HandlerType::AfterParse ||
+                handler.handlerType() == Configuration::HandlerType::Generator ||
                 handler.handlerType() == Configuration::HandlerType::Validator) {
                 handler.section(tmp, _axis[a]);
             }